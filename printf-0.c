0099898a0998c9e61a5570cdde0efe5c3cbfd23d
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-01-30 05:37:47 -0800

    added some code snippets

--- printf-0.c
+++ printf-0.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
 
-int main(void)
+int myprintf(const char *format, ...)
 {
-	int _printf(const char *format, ...);	
-	va_list args;
-	va_start(args, format);
+    va_list args;
+    va_start(args, format);
+    int count = 0;
+    while (*format != '\0')
+    {
+        if (*format != '%')
+        {
+            putchar(*format);
+            count++;
+        }
+        else
+        {
+            switch (*++format)
+            {
+            case 'd':
+                count += fprintf(stdout, "%d", va_arg(args, int));
+                break;
+            case 'c':
+                count += fprintf(stdout, "%c", va_arg(args, char));
+                break;
+            case 's':
+                count += fprintf(stdout, "%s", va_arg(args, char *));
+                break;
+            case 'f':
+                count += fprintf(stdout, "%f", va_arg(args, double));
+                break;
+            default:
 
-	int count = 0;
-
-	return (0);
+                putchar('%');
+                putchar(*format);
+                count += 2;
+                break;
+            }
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
 }
