7392c1827783f92e108ec21945f0cc8fd475f4b6
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-01-30 10:46:47 -0800

    Created the printf code

--- printf-0.c
+++ printf-0.c
@@ -3,42 +3,41 @@
 
 int myprintf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
-    while (*format != '\0')
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            count++;
-        }
-        else
-        {
-            switch (*++format)
-            {
-            case 'd':
-                count += fprintf(stdout, "%d", va_arg(args, int));
-                break;
-            case 'c':
-                count += fprintf(stdout, "%c", va_arg(args, char));
-                break;
-            case 's':
-                count += fprintf(stdout, "%s", va_arg(args, char *));
-                break;
-            case 'f':
-                count += fprintf(stdout, "%f", va_arg(args, double));
-                break;
-            default:
+	va_list args;
+	va_start(args, format);
 
-                putchar('%');
-                putchar(*format);
-                count += 2;
-                break;
-            }
-        }
-        format++;
-    }
-    va_end(args);
-    return count;
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			count++;
+	} else
+		{
+			switch (*++format)
+			{
+				case 'c':
+					count += fprintf(stdout, "%c", va_arg(args, int));
+					break;
+				case 's':
+					count += fprintf(stdout, "%s", va_arg(args, char *));
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
