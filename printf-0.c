be31dde401ba63d11590eb648cb00b04dfdfdb6a
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-01-31 05:24:11 -0800

    created some code snippets and made some changes

--- printf-0.c
+++ printf-0.c
@@ -34,6 +34,10 @@ int _printf(const char *format, ...)
 			case 's':
 				count += fprintf(stdout, "%s", va_arg(args, char *));
 				break;
+			case 'd':
+			case 'i':
+				count += fprintf(stdout, "%d", va_arg(args, int));
+				break;
 			case '%':
 				count += fprintf(stdout, "%%");
 				break;
