fd4216072f1fc90b85c3b8bd066b98f2d9c79b4a
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-01-31 05:12:18 -0800

    worked on the files

--- printf-0.c
+++ printf-0.c
@@ -3,117 +3,44 @@
 #include <stdarg.h>
 
 /**
- * _putchar - prints a character to stdout
- * @c: The character to print
- * Return: The number of characters printed (always 1)
- */
-int _putchar(char c)
-{
-	putchar(c);
-	return (1);
-}
-
-/**
- * print_char - prints a character
- * @args: The argument list
- * @count: The count of characters printed
- * Return: The updated count of characters printed
- */
-int print_char(va_list args, int count)
-{
-	char *str = va_arg(args, char*);
-
-	if (str == NULL)
-		str = "(null)";
-
-	while (*str)
-	{
-		count += _putchar(*str++);
-	}
-
-	return (count);
-}
-
-/**
- * print_str - prints a string
- * @args: The argument list
- * @count: The count of characters printed
- * Return: The updated count of characters printed
- */
-int print_str(va_list args, int count)
-{
-	char *str = va_arg(args, char*);
-
-	if (str == NULL)
-		str = "(null)";
-
-	while (*str)
-	{
-		count += _putchar(*str++);
-	}
-
-	return (count);
-}
-
-/**
- * print_integer - prints an integer
- * @args: The argument list
- * @count: The count of characters printed
- * Return: The updated count of characters printed
- */
-int print_integer(va_list args, int count)
-{
-	int value = va_arg(args, int);
-	count += fprintf(stdout, "%d", value);
-	return (count);
-}
-
-/**
- * _printf - custom printf function that supports a subset of
- * format specifiers.
+ * myprintf - Custom printf function with limited functionality.
  * @format: The format string.
- * @...: variable arguments to be formatted according to the format strings.
- * Return: the number of characters printed to the console.
+ * @...: Variable arguments.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
+
 	int count = 0;
 
 	va_start(args, format);
 
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format != '%')
-			count += _putchar(*format);
+		{
+			fputc(*format, stdout);
+			count++;
+		}
 		else
 		{
-			format++;
-			if (*format == '\0')
-				break;
-
-			switch (*format)
+			switch (*++format)
 			{
 			case 'c':
-				count = print_char(args, count);
+				count += fprintf(stdout, "%c", va_arg(args, int));
 				break;
-
-			case 'd':
-			case 'i':
-				count = print_integer(args, count);
-				break;
-
 			case 's':
-				count = print_str(args, count);
+				count += fprintf(stdout, "%s", va_arg(args, char *));
 				break;
-
 			case '%':
-				count += _putchar('%');
+				count += fprintf(stdout, "%%");
 				break;
-
 			default:
-				count += _putchar('%');
-				count += _putchar(*format);
+				fputc('%', stdout);
+				fputc(*format, stdout);
+				count += 2;
 				break;
 			}
 		}
