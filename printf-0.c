b3c9ec606b7d9f8ee5fd90a78e56df584bc8be84
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-02-02 19:14:43 -0800

    added some chuncks of code to this file

--- printf-0.c
+++ printf-0.c
@@ -34,6 +34,9 @@ int _printf(const char *format, ...)
 			case 's':
 				count += fprintf(stdout, "%s", va_arg(args, char *));
 				break;
+			case 'b'
+				count += fprint(stdout, "%b", va_arg(args, int));
+				break;
 			case 'd':
 			case 'i':
 				count += fprintf(stdout, "%d", va_arg(args, int));
